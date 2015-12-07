415243c667cf648b8f7d0e5403673e536e2012bc
spacedmi <spacedmi@gmail.com>
2015-12-07 17:53:20 +0300

    Added Field interface and started field classes implementing

---
+++ DotNET/LMI/LMI/Fields/D4Field.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMI.Fields
+{
+    class D4Field : IField
+    {
+        public double Angle { get; set; }
+
+        public void Draw()
+        {
+            // TODO: D4 Draw
+        }
+    }
+}
---
+++ DotNET/LMI/LMI/Fields/D5Field.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMI.Fields
+{
+    class D5Field : IField
+    {
+        public double Mu1 { get; set; }
+        public double Mu2 { get; set; }
+
+        public void Draw()
+        {
+            // TODO: D5 Draw
+        }
+    }
+}
