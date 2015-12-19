e1f2283f3cab8d0f1ce243155b46b9c22b7ab5ce
spacedmi <spacedmi@gmail.com>
2015-12-19 12:45:16 +0300

    D1, D2, D3, D5 solving

--- Python/LMI.py
+++ Python/LMI.py
@@ -1,5 +1,5 @@
 import json
-from sympy import symbols, Matrix, sin, cos, zeros, ones
+import sympy as sm
 from lmi_sdp import LMI_PD, LMI_ND
 from cvxopt import solvers
 from lmi_sdp import to_cvxopt
@@ -11,8 +11,28 @@ import numpy as np
 # for field in fields:
 # 	print(field['Name'])
 
+def GetBlockMatrix(A, B, C, D, dim):
+	M = sm.zeros(dim, dim)
+
+	for i in xrange(0, dim / 2, 1):
+		for j in xrange(0, dim / 2, 1):
+			M[i, j] = A[i, j]
+
+	for i in xrange(0, dim / 2, 1):
+		for j in xrange(0, dim / 2, 1):
+			M[i, dim / 2 + j] = B[i, j]
+
+	for i in xrange(0, dim / 2, 1):
+		for j in xrange(0, dim / 2, 1):
+			M[ dim / 2 + i, j] = C[i, j]
+
+	for i in xrange(0, dim / 2, 1):
+		for j in xrange(0, dim / 2, 1):
+			M[ dim / 2 + i, dim / 2 + j] = D[i, j]
+	return M;
+
 N = 3
-A = Matrix([[-21, -11, 0], [-11, 10, 8], [0, 8, 5]])
+A = sm.Matrix([[-21, -11, 0], [-11, 10, 8], [0, 8, 5]])
 mu = 3.0
 r = 5.0
 q = 10.0
@@ -25,8 +45,8 @@ s = 0
 for i in xrange(0, N + 1, 1):
     s = s + i
 
-x = [symbols('x' + str(i)) for i in xrange(0, s, 1)]
-Iks = zeros(N, N)
+x = [sm.symbols('x' + str(i)) for i in xrange(0, s, 1)]
+Iks = sm.zeros(N, N)
 
 k = 0;
 for i in xrange(0, N, 1):
@@ -35,20 +55,31 @@ for i in xrange(0, N, 1):
         Iks[j, i] = x[k]
         k += 1
 
-print(r *Iks * r *Iks - ((q *Iks + A *Iks) * (q *Iks +Iks * A.transpose())))
-lmi_1 = LMI_ND(A *Iks +Iks * A.transpose() + 2 * mu *Iks, 0)
-lmi_2 = LMI_ND(-r *Iks, 0)
-lmi_3 = LMI_PD(r *Iks * r *Iks - ((q *Iks + A *Iks) * (q *Iks +Iks * A.transpose())), 0)
-# lmi_4 = LMI_ND(-2 * nu *Iks, 0)
-# lmi_5 = LMI_PD(4 * nu * nu *Iks *Iks - (A *Iks -Iks * A.transpose()) * (-A *Iks +Iks * A.transpose()), 0)
-# lmi_6 = LMI_ND((A *Iks +Iks * A.transpose()) * sin(fi), 0)
-# lmi_7 = LMI_PD((A *Iks +Iks * A.transpose()) * sin(fi) * (A *Iks +Iks * A.transpose()) * sin(fi) - ((A *Iks -Iks * A.transpose()) * cos(fi) * (A *Iks -Iks * A.transpose()) * cos(fi)), 0)
-# lmi_8 = LMI_ND(A *Iks +Iks * A.transpose() + 2 * mu1 *Iks, 0)
-# lmi_9 = LMI_PD((A *Iks +Iks * A.transpose() + 2 * mu1 *Iks) * (-A *Iks -Iks * A.transpose() - 2 * mu2 *Iks), 0)
+print(GetBlockMatrix(-r * Iks, q * Iks + A * Iks, q * Iks + Iks * A.transpose(), -r * Iks, 2 * N))
+lmi_1_matrix = A * Iks +Iks * A.transpose() + 2 * mu * Iks;
+lmi_2_matrix = GetBlockMatrix(-r * Iks, q * Iks + A * Iks, q * Iks + Iks * A.transpose(), -r * Iks, 2 * N)
+lmi_3_matrix = GetBlockMatrix(-2 * nu * Iks, A * Iks - Iks * A.transpose(), -A * Iks + Iks * A.transpose() , -2 * nu * Iks, 2 * N);
+lmi_4_matrix = GetBlockMatrix((A * Iks + Iks * A.transpose()) * sm.sin(fi), (A * Iks - Iks * A.transpose()) * sm.cos(fi),
+	(A * Iks - Iks * A.transpose()) * sm.cos(fi), (A * Iks + Iks * A.transpose()) * sm.sin(fi), 2 * N);
+lmi_5_matrix = GetBlockMatrix(A * Iks + Iks * A.transpose() + 2 * mu1 * Iks, sm.zeros(N, N), 
+	sm.zeros(N, N), -A * Iks - Iks * A.transpose() - 2 * mu2 * Iks, 2 * N);
 
-min_obj = x[1]
+lmi_1 = LMI_ND(lmi_1_matrix, sm.zeros(N, N))
+lmi_2 = LMI_ND(lmi_2_matrix, sm.zeros(2 * N, 2 * N))
+lmi_3 = LMI_ND(lmi_3_matrix, sm.zeros(2 * N, 2 * N))
+# lmi_4 = LMI_ND(lmi_4_matrix, sm.zeros(2 * N, 2 * N))
+lmi_5 = LMI_ND(lmi_5_matrix, sm.zeros(2 * N, 2 * N))
+# lmi_3 = LMI_PD(r * Iks * r * Iks - ((q * Iks + A * Iks) * (q * Iks + Iks * A.transpose())), 0)
+# lmi_4 = LMI_ND(-2 * nu * Iks, 0)
+# lmi_5 = LMI_PD(4 * nu * nu * Iks * Iks - (A * Iks -Iks * A.transpose()) * (-A * Iks +Iks * A.transpose()), 0)
+# lmi_6 = LMI_ND((A * Iks +Iks * A.transpose()) * sm.sin(fi), 0)
+# lmi_7 = LMI_PD((A * Iks +Iks * A.transpose()) * sm.sin(fi) * (A * Iks +Iks * A.transpose()) * sm.sin(fi) - ((A * Iks -Iks * A.transpose()) * sm.cos(fi) * (A * Iks -Iks * A.transpose()) * sm.cos(fi)), 0)
+# lmi_8 = LMI_ND(A * Iks +Iks * A.transpose() + 2 * mu1 * Iks, 0)
+# lmi_9 = LMI_PD((A * Iks +Iks * A.transpose() + 2 * mu1 * Iks) * (-A * Iks -Iks * A.transpose() - 2 * mu2 * Iks), 0)
+
+min_obj = x[0]
 solvers.options['show_progress'] = False
-c, Gs, hs = to_cvxopt(min_obj, [lmi_1, lmi_2, lmi_3], x)
+c, Gs, hs = to_cvxopt(min_obj, [lmi_1, lmi_2, lmi_3, lmi_5], x)
 
 sol = solvers.sdp(c, Gs = Gs, hs = hs)
 print(sol['x'])
